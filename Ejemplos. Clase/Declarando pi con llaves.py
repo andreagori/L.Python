e56136bf021ed73fa816f29f81ee0a88efe834bf
andreagori <andrearivassg@gmail.com>
2024-02-13 07:51:35 -0800

    python

---
+++ Ejemplos. Clase/Declarando pi con llaves.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 13 07:24:06 2024
+
+@author: andyt
+"""
+
+import math
+print(f'El valor de pi es aproximadamente {math.pi:.4f}.')
+
+# help(). Comando para pedir ayuda.
