bb19915ed1e08fbb52085f31c162d16f35f320ee
andreagori <andrearivassg@gmail.com>
2024-05-25 13:16:47 -0700

    ExposicionPygames

--- Ejercicios/videojuegosPygame.py
+++ Ejercicios/videojuegosPygame.py
@@ -1,4 +1,5 @@
 # from random import randint # libreria para generar numeros aleatorios.
+"""
 import pgzrun # libreria para crear videojuegos.
 
 # Minigame 1: car racing
@@ -19,7 +20,6 @@ def draw_racing_line():
     return racing_line
 
 def racing_collision(racing_car, racing_line_x):
-    """Function to check collision with the finish racing line."""
     if racing_car.x >= racing_line_x:
         print("You won!")
         quit()
@@ -36,32 +36,61 @@ def update():
     racing_collision(racing_car, racing_line_x)
 
 pgzrun.go()
+"""
+# Minigame 2: two player game
+import pgzrun
+from pgzero.actor import Actor
+from pgzero.rect import Rect
+from pgzero.screen import Screen
 
-# def draw():
-#     screen.clear()
-#     screen.draw.text("Hola Mundo", topleft=(10, 10), fontsize=60)
+# Variables
+racing_car1 = Actor("car")
+racing_car1.pos = 150, 300  # x, y
 
-# pgzrun.go()
-# manzana = Actor("manzanita")
+racing_car2 = Actor("car")
+racing_car2.pos = 150, 350  # x, y
 
-# def draw():
-#     """Function to draw the apple."""
-#     screen.clear()
-#     manzana.draw()
+finish_line_x = 500
+winner_declared = False
 
-# def place_apple():
-#     """Function random apple position."""
-#     manzana.x = randint(10, 800)
-#     manzana.y = randint(10, 600)
 
-# def on_mouse_down(pos):
-#     """Function to follow the mouse."""
-#     if manzana.collidepoint(pos):
-#         print("Good shot!")
-#         place_apple()
-#     else:
-#         print("You missed!")
-#         quit()
+# Funciones
+def draw():
+    screen.clear()
+    screen.fill((128, 128, 128))
+    draw_racing_line()
+    racing_car1.draw()
+    racing_car2.draw()
 
-# place_apple()
-# pgzrun.go()
+
+def draw_racing_line():
+    screen.draw.filled_rect(Rect((finish_line_x, 100, 50, 400)), (125, 52, 65))
+
+
+def racing_collision(racing_car, racing_line_x):
+    """Function to check collision with the finish racing line."""
+    return racing_car.x >= racing_line_x
+
+
+def on_key_down(key):
+    global winner_declared
+    if winner_declared:
+        return
+
+    if key == keys.UP:
+        racing_car1.x += 20
+        if racing_collision(racing_car1, finish_line_x):
+            print("Player 1 won!")
+            winner_declared = True
+    elif key == keys.W:
+        racing_car2.x += 20
+        if racing_collision(racing_car2, finish_line_x):
+            print("Player 2 won!")
+            winner_declared = True
+
+
+def update():
+    pass
+
+
+pgzrun.go()
